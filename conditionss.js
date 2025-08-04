cc2a1c31e1bd63c540994523f262629a2e534b6e
Aryan Gupta <aryan11gs@gmail.com>
2025-08-04 13:45:11 +0530

    JS Basics

---
+++ conditionss.js
@@ -0,0 +1,25 @@
+const flag=true
+
+if(!flag){
+console.log("Condition Satisfied")
+}
+else{
+    console.log("Not Satisfied")
+}
+
+
+let i =1
+while(i<10){
+    i++
+    console.log(i,"am inside loop")
+    
+}
+
+do{
+    console.log(i,"am inside do-while loop")
+    i++
+}
+while(i>10);{
+    i++    
+    console.log(i)
+}
---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+let a=4
+console.log(a)
+console.log(typeof(a))
+
+let required=true
+console.log(typeof(required))
+
+console.log(!required)
